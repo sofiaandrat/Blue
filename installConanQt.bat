a6a61715e7292ca834d4afc76b176ff6938b9d64
Baranov-Ivan <vano95by@gmail.com>
2019-11-19 16:36:16 +0300

    Build project

---
+++ installConanQt.bat
@@ -0,0 +1,3 @@
+conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-conan"
+conan remote add bintray "https://bintray.com/bincrafters/public-conan"
+conan install conanfile.txt --build sqlite3
