efad3daf2bd3bcd660d3efff578c582d447e9694
Sofia Anisimova <sofiaanisimova@yandex.by>
2019-11-20 01:07:06 +0300

    I hope last

--- mainwindow.h
+++ mainwindow.h
@@ -1,17 +1,8 @@
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-<<<<<<< HEAD
-#include <QJsonObject>
-#include <QJsonArray>
-#include <QJsonParseError>
-#include <QFile>
-#include <QFileDialog>
-#include <QStandardItem>
-#include <QMessageBox>
-=======
->>>>>>> First commit
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -25,21 +16,8 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-<<<<<<< HEAD
-    QJsonDocument doc;
-    QJsonArray docAr;
-    QJsonParseError docError;
-
-    QString globpath;
-    QFile file;
-
-
-private slots:
-    void on_actionOpen_json_file_triggered();
-=======
 private slots:
     void on_pushButton_clicked();
->>>>>>> First commit
 
 private:
     Ui::MainWindow *ui;
